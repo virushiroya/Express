f4e7a1329875f8c130e017d70c5677fbad02ad62
virushiroya <virushiroya261@GMAIL.COM>
2024-08-27 22:50:16 +0530

    27-08-24

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Express');
+    res.status(200);
+    res.end();
+});
+app.get('/login', (req, res) => {
+    res.json({ msg: 'Welcome to Login Page' });
+    res.end();
+});
+app.post('/login', (req, res) => {
+    res.send('Welcome to Login Post method');
+    res.end();
+});
+app.listen(2304, () => {
+    console.log('Server is connected at http://localhost:2304');
+});
